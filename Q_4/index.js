202492166f64c19af90afb034034f1078a0e40ec
alishhaKhan <alishakhan8627@gmail.com>
2024-04-14 01:35:33 +0500

    Q_4 commit

---
+++ Q_4/index.js
@@ -0,0 +1,5 @@
+let qoute = "The only way to do great work is to love what you do";
+let famous_person = "Steve Jobs";
+let message = `${famous_person} once said, ${qoute}`;
+console.log(message);
+export {};
---
+++ Q_4/index.ts
@@ -0,0 +1,4 @@
+let qoute : string = "The only way to do great work is to love what you do";
+let famous_person ="Steve Jobs";
+let message = `${famous_person} once said, ${qoute}`;
+console.log(message);
