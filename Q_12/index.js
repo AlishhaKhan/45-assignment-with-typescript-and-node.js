8484ba87499373a6afd6566f322046dfdd224b63
alishhaKhan <alishakhan8627@gmail.com>
2024-04-14 02:34:24 +0500

    Q_12 commit

---
+++ Q_12/index.js
@@ -0,0 +1,5 @@
+let guest_list = ['tim cook', 'elon musk', 'mark zukerberg', 'bill gtes'];
+for (let i = 0; i < guest_list.length; i++) {
+    console.log('respected' + guest_list[i] + ',\nI would like to invite you on dinner tomorrow\nThank You\n');
+}
+export {};
---
+++ Q_12/index.ts
@@ -0,0 +1,4 @@
+let guest_list : string[] = ['tim cook','elon musk','mark zukerberg','bill gtes'];
+for ( let i=0; i<guest_list.length;  i++){
+      console.log('respected' + guest_list[i] + ',\nI would like to invite you on dinner tomorrow\nThank You\n');
+}
