e9037142f886a685ddbb8277e5d41cdbf5d0cdad
alishhaKhan <alishakhan8627@gmail.com>
2024-04-14 01:42:41 +0500

    Q_5 commit

---
+++ Q_5/index.js
@@ -0,0 +1,5 @@
+let personName = `\n\tAlisha\t\n`;
+console.log(personName);
+let stripped = personName.trimEnd();
+console.log(stripped);
+export {};
---
+++ Q_5/index.ts
@@ -0,0 +1,4 @@
+let personName : string =`\n\tAlisha\t\n`;
+console.log(personName);
+let stripped : string = personName.trimEnd();
+console.log(stripped);
