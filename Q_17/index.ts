074252fba26ca194bb5783b6031d41a1f547306a
alishhaKhan <alishakhan8627@gmail.com>
2024-04-14 03:08:14 +0500

    Q_17 commit

---
+++ Q_17/index.ts
@@ -0,0 +1,29 @@
+// store loction in your array
+let places : string[] = ['Makkah','Toronto','Paris','India'];
+
+//original order
+console.log('original' + places);
+
+//alphabetrical order without modifying
+console.log('copy'  + [...places].sort());
+
+//still in its original order
+console.log('original' + places);
+
+//in reverse alphabetrical order
+console.log('copy'  + [...places].sort().reverse());
+
+//still in its original order
+console.log('original' + places);
+
+////in reverse alphabetrical order, print and show its order has changed
+console.log('original'  + places.reverse());
+
+//in reverse alphabetrical order, print to show its back in its original order
+console.log('original'  + places.reverse());
+
+//sort your arry so its store in alphabetrical order
+console.log('original' + places.sort());
+
+//sort to change your array so its store in reverse alphabetrical order
+console.log('original' + places.sort().reverse());
