4f5b976e35d369a96058d654ca117cd308d4fe25
alishhaKhan <alishakhan8627@gmail.com>
2024-04-14 02:07:48 +0500

    Q_8 commit

---
+++ Q_8/index.js
@@ -0,0 +1,6 @@
+// Exercise 10
+// My name is Alisha
+// Dated : 03/04/2024
+// This progrm will run simple code just like hello world
+console.log(`hello world`);
+export {};
---
+++ Q_8/program2.ts
@@ -0,0 +1,5 @@
+// Exercise 10
+// My name is Alisha
+// Time : 04:54 pm
+// This program may run multiplication
+console.log(5*2);
