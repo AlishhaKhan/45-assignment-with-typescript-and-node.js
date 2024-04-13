7eefb7366c92069e61f89e036213e4dd415717a9
alishhaKhan <alishakhan8627@gmail.com>
2024-04-14 03:02:05 +0500

    Q_16 commit

---
+++ Q_16/index.ts
@@ -0,0 +1,18 @@
+let guest_list : string[] = ['Areej','Iraj','Hamzah'];
+
+let not_present : string = 'Hamzah';
+let new_guest : string = 'Orhi';
+guest_list[1] = new_guest;
+
+guest_list.unshift('Rehan', 'Hamna','Esha');
+console.log('\nUnfortunately we cannot arrange big table, only two people allow.');
+while(guest_list.length>2){
+    let remove_guest = guest_list.pop();
+console.log(`Sorry Sir/Madam, ${remove_guest} you are not invited for dinner.`);
+}
+
+for(let i=0; i<guest_list.length;  i++){
+    console.log('Respected Sir/Madam ' + guest_list[1] + ',\nYou are still invited on tomorrow dinner.\nThank You\n')
+} 
+guest_list.splice(0,2);
+console.log(guest_list);
