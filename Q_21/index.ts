56a8a06b43f47b71718a229609923051b5560a63
alishhaKhan <alishakhan8627@gmail.com>
2024-04-14 04:14:02 +0500

    Q_21 commit

---
+++ Q_21/index.ts
@@ -0,0 +1,5 @@
+// let array : (string | number) [] = ['Oliver','Jennifer', 8 , 6 , 7];
+// console.log(array[5]);
+
+let array : (string | number) [] = ['Oliver','Jennifer', 8 , 9 , 7 , 'Orhi'];
+console.log(array[5]);
