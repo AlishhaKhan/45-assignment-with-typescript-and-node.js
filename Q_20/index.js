fc11ce3094802a2af5b418bf5a6c1b4aa96e4417
alishhaKhan <alishakhan8627@gmail.com>
2024-04-14 04:08:16 +0500

    Q_20 commit

---
+++ Q_20/index.js
@@ -0,0 +1,16 @@
+//person object
+let person = {
+    age: 22,
+    name: 'Alisha',
+    nationality: 'Pakistani',
+    student: true
+};
+console.log(person);
+//car object
+let car = {
+    model: 'Toyota',
+    color: 'Black',
+    automatic: true
+};
+console.log(car);
+export {};
---
+++ Q_20/index.ts
@@ -0,0 +1,26 @@
+// write a program that creates objects contining these items
+interface person {
+    age : number,
+    name : string
+    nationality : string,
+    student : boolean,
+}
+
+//person object
+let person : person = {
+
+   age : 22,
+   name : 'Alisha',
+   nationality : 'Pakistani',
+   student : true
+}
+console.log(person);
+
+//car object
+let car = {
+
+    model : 'Toyota',
+    color : 'Black',
+    automatic : true
+}
+console.log(car);
