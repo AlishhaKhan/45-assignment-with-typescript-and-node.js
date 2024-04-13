af2c9fafc4f47d0347ec0caf3709508bce151d0d
alishhaKhan <alishakhan8627@gmail.com>
2024-04-14 02:40:45 +0500

    Q_13 commit

---
+++ Q_13/index.js
@@ -0,0 +1,12 @@
+let guest_list = ['tim cook', 'elon musk', 'bill gates', 'mark zuckerberg'];
+for (let i = 0; i < guest_list.length; i++) {
+    console.log('respectedly' + guest_list[i] + ',\nI would like to invite you on dinner tomorrow.\n\nThnk You\n');
+}
+let not_person = 'mark zuckerberg';
+let new_person = 'mr. beast';
+guest_list[1] = new_person;
+for (let i = 0; i < guest_list.length; i++) {
+    console.log('respected' + guest_list[i] + ',\nI would like to invite you on dinner tomorrow.\nThank You\n');
+}
+console.log('Mr.${not_present}  will not comming tomorrow.');
+export {};
