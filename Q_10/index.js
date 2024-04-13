e8d2a784c2f3d861da3c5a9c88525a0fa0af2cf9
alishhaKhan <alishakhan8627@gmail.com>
2024-04-14 02:23:31 +0500

    Q_10 commit

---
+++ Q_10/index.js
@@ -0,0 +1,6 @@
+let names = ['Umar', 'Saim', 'Sarim', 'Alishba'];
+const message = '\tHow are you?';
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i] + message);
+}
+export {};
---
+++ Q_10/index.ts
@@ -0,0 +1,6 @@
+let names: string[] = ['Umar','Saim','Sarim','Alishba'] ;
+const message :string ='\tHow are you?'
+
+for(let i=0 ; i<names.length ;i++){
+    console.log(names[i] + message);
+}
