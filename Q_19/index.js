e586a2219d37c65aab069607bcf6a645833a06bc
alishhaKhan <alishakhan8627@gmail.com>
2024-04-14 04:02:19 +0500

    Q_19 commit

---
+++ Q_19/index.js
@@ -0,0 +1,10 @@
+//think of something you could store in array
+let items = ['Pakistan', 'Karachi', 'Urdu'];
+//write a program that creates a list containing these items
+let item2 = [];
+item2.push('Makkah');
+item2.push('California');
+item2.push('Madina');
+//we use push property to store strings in array
+console.log(item2);
+export {};
---
+++ Q_19/index.ts
@@ -0,0 +1,11 @@
+//think of something you could store in array
+let items : string [] = ['Pakistan','Karachi','Urdu'];
+
+//write a program that creates a list containing these items
+let item2 : string[] = [];
+item2.push('Makkah');
+item2.push('California');
+item2.push('Madina');
+
+//we use push property to store strings in array
+console.log(item2);
