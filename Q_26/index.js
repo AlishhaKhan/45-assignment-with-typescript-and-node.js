922b4709d5210376ce55273b714cdf7dbf342fd8
alishhaKhan <alishakhan8627@gmail.com>
2024-04-14 05:22:06 +0500

    Q_26 commit

---
+++ Q_26/index.js
@@ -0,0 +1,15 @@
+let alien_color = "green";
+if (alien_color === "green") {
+    console.log("player just earned 5 points for shooting the alien");
+}
+else {
+    console.log("player just earned 10 points!");
+}
+let alien_colors = "yellow";
+if (alien_colors === "green") {
+    console.log("player just earned 5 points for shooting the aliens.");
+}
+else {
+    console.log("player just earned 10 points!");
+}
+export {};
---
+++ Q_26/index.ts
@@ -0,0 +1,14 @@
+let alien_color : string = "green";
+if(alien_color === "green"){
+    console.log("player just earned 5 points for shooting the alien");
+}
+else{
+    console.log("player just earned 10 points!");
+}
+
+let alien_colors = "yellow";
+if(alien_colors === "green"){
+    console.log("player just earned 5 points for shooting the aliens.");
+}else{
+    console.log("player just earned 10 points!");
+}
