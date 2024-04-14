2db633709f0037f943f19ae30916dcbd0a97296d
alishhaKhan <alishakhan8627@gmail.com>
2024-04-15 04:46:53 +0500

    Q_27 commit

---
+++ Q_27/index.ts
@@ -0,0 +1,44 @@
+let alien_color : string = "green";
+// Version 1 of program :
+if(alien_color === "green"){
+    console.log("player earned 5 points");
+}
+else if(alien_color === "yellow") {
+    console.log("player earned 10 points!");
+}
+ else if(alien_color === "red"){
+    console.log("player earned 15 points.");
+}
+else{
+    console.log("please select right color.");
+}
+
+// Version 2 of program :
+alien_color = "yellow";
+if(alien_color === "green"){
+    console.log("player earned 5 points");
+}
+else if(alien_color === "yellow") {
+    console.log("player earned 10 points!");
+}
+ else if(alien_color === "red"){
+    console.log("player earned 15 points.");
+}
+else{
+    console.log("please select right color.");
+}
+
+// Version 3 of program :
+alien_color = "red";
+if(alien_color === "green"){
+    console.log("player earned 5 points");
+}
+else if(alien_color === "yellow") {
+    console.log("player earned 10 points!");
+}
+ else if(alien_color === "red"){
+    console.log("player earned 15 points.");
+}
+else{
+    console.log("please select right color.");
+}
