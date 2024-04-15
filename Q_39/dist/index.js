9d88196330f6ce13a135d31a175a22fce4d54195
alishhaKhan <alishakhan8627@gmail.com>
2024-04-15 07:55:43 +0500

    Q_39 commit

---
+++ Q_39/dist/index.js
@@ -0,0 +1,7 @@
+function city_country(city, country) {
+    return `${city},${country}`;
+}
+console.log(city_country('karachi', 'Pakistan'));
+console.log(city_country('Tokyo', 'Japan'));
+console.log(city_country('Paris', 'France'));
+export {};
