42984d03b6020bfcdf31e3a7fc2854020b835cb4
alishhaKhan <alishakhan8627@gmail.com>
2024-04-15 05:14:56 +0500

    Q_29 commit

---
+++ Q_29/index.ts
@@ -0,0 +1,13 @@
+let favouriteFruit : string[] = ['apple','grapes','strawberry'];
+if(favouriteFruit.includes('strawberry')){
+    console.log("I really like strawberry!");
+}
+if(favouriteFruit.includes('grapes')){
+    console.log("I really like grapes!");
+}
+if(favouriteFruit.includes('apple')){
+    console.log("I really like apple!");
+}
+if(favouriteFruit.includes('orange')){
+    console.log("I really like orange!");
+}
