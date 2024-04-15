1ee2ee672602f4f352bb89439e3242326840c8a2
alishhaKhan <alishakhan8627@gmail.com>
2024-04-15 06:18:28 +0500

    Q_30 commit

---
+++ Q_30/dist/index.js
@@ -0,0 +1,10 @@
+const userNames = ['admin', 'Jennifer', 'Areej', 'Daniya', 'Ayan', 'Umal'];
+for (let i = 0; i < userNames.length; i++) {
+    if (userNames[i] === 'admin') {
+        console.log('Hello admin, would you like to see status report');
+    }
+    else {
+        console.log(`Hello ${userNames[i]} thank you for logging in again.`);
+    }
+}
+export {};
