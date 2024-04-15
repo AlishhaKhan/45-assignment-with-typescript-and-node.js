dd6b4099ae437ec7640248445aa4bcd90ca09ac3
alishhaKhan <alishakhan8627@gmail.com>
2024-04-15 05:00:22 +0500

    Q_28 commit

---
+++ Q_28/index.js
@@ -0,0 +1,17 @@
+let age = 22;
+if (age < 2) {
+    console.log("Person is a baby");
+}
+else if (age >= 4 && age < 13) {
+    console.log("Person is a kid");
+}
+else if (age >= 13 && age < 20) {
+    console.log("Person is a teenager");
+}
+else if (age >= 20 && age < 65) {
+    console.log("Person is an adult");
+}
+else {
+    console.log("Person is an elder");
+}
+export {};
