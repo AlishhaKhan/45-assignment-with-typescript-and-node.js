e8c6a9556cabcc4e4833b5935eb3ca3c23b61010
alishhaKhan <alishakhan8627@gmail.com>
2024-04-15 07:31:00 +0500

    Q_36 commit

---
+++ Q_36/dist/index.js
@@ -0,0 +1,7 @@
+function make_shirt(size, text) {
+    console.log(`create a ${size} shirt with the message : ${text}`);
+}
+make_shirt('Large', 'Hello World');
+make_shirt('Small', 'Hello World');
+make_shirt('medium', 'Hello World');
+export {};
---
+++ Q_36/src/index.ts
@@ -0,0 +1,6 @@
+function make_shirt(size : string, text : string){
+  console.log(`create a ${size} shirt with the message : ${text}`);
+} 
+make_shirt('Large', 'Hello World');
+make_shirt('Small', 'Hello World');
+make_shirt('medium', 'Hello World');
