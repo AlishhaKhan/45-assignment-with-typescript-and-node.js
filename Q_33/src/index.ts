b1f8ec254a10ed088dc81fdde16054d302e40beb
alishhaKhan <alishakhan8627@gmail.com>
2024-04-15 07:10:00 +0500

    Q_33 commit

---
+++ Q_33/src/index.ts
@@ -0,0 +1,17 @@
+let numbers : number[] = [1,2,3,4,5,6,7,8,9];
+for(let num of numbers){
+  let ordinlEnding : string;
+  if(num === 1){
+    ordinlEnding = 'st';
+  }
+  else if(num === 2){
+    ordinlEnding = 'nd';
+  }
+  else if(num === 3){
+    ordinlEnding = 'rd';
+  }
+  else{
+    ordinlEnding = 'th';
+  }
+  console.log(num + ordinlEnding);
+}
