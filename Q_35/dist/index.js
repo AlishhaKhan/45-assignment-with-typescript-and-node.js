21f3d3f933904071119be8faaf6d32a59d3cbe81
alishhaKhan <alishakhan8627@gmail.com>
2024-04-15 07:23:07 +0500

    Q_35 commit

---
+++ Q_35/dist/index.js
@@ -0,0 +1,6 @@
+let animals = ['dog', 'rabbit', 'cat'];
+for (let animal of animals) {
+    console.log(`A ${animal} would make a great pet`);
+}
+console.log(`Any of these animals would make a great pet.`);
+export {};
