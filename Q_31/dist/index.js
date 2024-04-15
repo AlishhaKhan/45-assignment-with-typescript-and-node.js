1d4d9134d5002e089ff3cc5a891965158ed136c6
alishhaKhan <alishakhan8627@gmail.com>
2024-04-15 06:26:52 +0500

    Q_31 commit

---
+++ Q_31/dist/index.js
@@ -0,0 +1,9 @@
+const userNames = ['admin', 'Jennifer', 'Areej', 'Daniya', 'Ayan', 'Umal'];
+if (userNames.length === 0) {
+    console.log('We need to find some users!');
+}
+else {
+    let userNames = [];
+    console.log('We need to find some users!' + userNames.length);
+}
+export {};
