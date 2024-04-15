c16f1e8e11d54d735a16230bf1a166d77841aba2
alishhaKhan <alishakhan8627@gmail.com>
2024-04-15 23:33:17 +0500

    Q_41 commit

---
+++ Q_41/dist/index.js
@@ -0,0 +1,8 @@
+let magician = ['Harry Potter', 'Heermoine Grnger', 'Ron Wesley', 'Albus Dumbledore'];
+function show_magicians(magician) {
+    magician.forEach(element => {
+        console.log(element);
+    });
+}
+show_magicians(magician);
+export {};
---
+++ Q_41/src/index.ts
@@ -0,0 +1,9 @@
+ let magician : string[] = ['Harry Potter','Heermoine Grnger','Ron Wesley','Albus Dumbledore'] ;
+ 
+ function show_magicians(magician : string[]){
+  magician.forEach(element => {
+    console.log(element);
+  });
+ }
+    show_magicians(magician);
+ 
